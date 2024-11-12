2ad10ad209eac51179e1218538b86630a59907e2
Choonsik00 <ittj2000@gmail.com>
2024-11-12 21:13:15 +0900

    11월 12일 공부한 내용

---
+++ 자바241112.java
@@ -0,0 +1,36 @@
+package java_study;
+
+public class 자바241112 {
+
+	public static void main(String[] args) {
+		// 여기에 코드를 작성
+		System.out.println("안녕하세요, 오늘 자바를 배웁니다.");
+		//ctrl+f11를 누르면 실행
+		
+		System.out.println("Hello world!");
+		
+		//syso -> Ctrl + space -> Enter System.out.println();를 빠르게 적어줌
+		System.out.println("으엥");
+		
+		System.out.println("현재 년도는: "+2024 + " 입니다.");
+		//출력문에서 +는 문자를 연결해준다는 의미
+		
+		System.out.println("키는: "+ 00 + " 입니다.");
+		System.out.println("나이는: "+ 00 + " 입니다.");
+		
+		//ln은 개행(줄바꿈)을 의미한다.
+		System.out.print("Hi");
+		System.out.print("Hi");
+		
+		//변수
+		int age = 20;
+		String address = "대전 서구 둔산동";
+		String mbti = "istj";
+		
+		System.out.println("나이는 : "+ age);
+		System.out.println("주소는 :"+ address);
+		System.out.println("MBTI :"+ mbti);
+		
+	}
+
+}

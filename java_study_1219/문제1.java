2b5942d8d23dd88a60d668020c3dd6e01fd75170
Choonsik00 <ittj2000@gmail.com>
2024-12-19 21:41:09 +0900

    Add files via upload

---
+++ java_study_1219/문제1.java
@@ -0,0 +1,24 @@
+package java_study_1219;
+
+class Animal {
+	   public void makeSound() {
+	      System.out.println("The animal makes a sound");
+	   }
+	}
+	class Dog extends Animal{
+	   
+	   @Override
+	   public void makeSound() {
+	      System.out.println("The dog barks");
+	   }
+	}
+
+
+public class 문제1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
